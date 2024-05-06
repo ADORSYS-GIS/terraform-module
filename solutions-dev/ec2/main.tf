0c389f6b380a94e3904620ad36cbca7a0407c6b8
Alexander Berdami <alexander.berdami@adorsys.com>
2024-05-06 11:23:52 +0200

    add debug code

--- solutions-dev/ec2/main.tf
+++ solutions-dev/ec2/main.tf
@@ -10,17 +10,17 @@ locals {
 
   user_data = <<-EOT
     #!/bin/bash
-    apt install apt-transport-https ca-certificates curl software-properties-common
+    apt-get install -y apt-transport-https ca-certificates curl software-properties-common
     curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
     add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
     apt-cache policy docker-ce
-    apt install docker-ce
+    apt-get install -y docker-ce
 
     curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
     chmod +x /usr/local/bin/docker-compose
 
     curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-    apt install unzip -y
+    apt-get install unzip -y
     unzip awscliv2.zip
     ./aws/install
 
