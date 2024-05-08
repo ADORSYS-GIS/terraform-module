5403427d41fedd17fa2a14a4a37bb5203eca8d7f
Alexander Berdami <alexander.berdami@adorsys.com>
2024-05-08 11:59:51 +0200

    change traefik router names to avoid conflicts

--- solutions-dev/ec2/main.tf
+++ solutions-dev/ec2/main.tf
@@ -27,6 +27,7 @@ locals {
     echo "export GITLABPW=$(aws ssm get-parameter --name "gitlab-registry-pw" --query "Parameter.Value" --with-decryption --output text)" >> $HOME/.bashrc
     source $HOME/.bashrc
 
+
     git clone -b watchtower https://groupaccesstoken:$TOKEN@git.adorsys.de/solutions/docker-develop
 
     echo "$GITLABPW" | docker login gitlab-registry.adorsys.de --username "$GITLABUSER" --password-stdin
